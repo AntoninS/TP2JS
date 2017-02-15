ef78f6e74c36b147ae62d174552d491a9e8be635
Valentin <valentin.guevara@etu.univ-lyon1.fr>
2017-02-15 18:02:50 +0100

    advancement

--- script.js
+++ script.js
@@ -7,9 +7,8 @@ function startTime() {
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 100);
 }
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
